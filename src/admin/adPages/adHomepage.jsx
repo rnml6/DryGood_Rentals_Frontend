53261aaa62dd2d7185b6ab62a7683236f6c91d3e
MaricarDLosSntos2 <maricardelossantos24@gmail.com>
2025-12-14 16:52:43 +0800

    day 1 maricar

---
+++ src/admin/adPages/adHomepage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Analysis from '../adComponents/analysis.jsx'
+
+function adHomepage () {
+  return (
+    <div>
+      <Analysis />
+    </div>
+  )
+}
+
+export default adHomepage
