216b7c12a2183e5c8a2dd6bcd1bdcf1934a9af7e
majo-123 <marjorie.carbonillabugua@gmail.com>
2025-12-15 12:00:20 +0800

    day 1 marjorie

---
+++ src/admin/adPages/adInventory.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ShowInventory from "../adComponents/showInventory.jsx";
+
+function adInventory() {
+  return (
+    <div>
+      <ShowInventory />
+    </div>
+  );
+}
+
+export default adInventory;
